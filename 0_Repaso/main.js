0a71db3c92384781aa2dd88e2fd335351dd265bf
Gera Medina <gerardo.mdn95@gmail.com>
2020-06-23 21:34:45 -0500

    add 0_Repaso

---
+++ 0_Repaso/main.js
@@ -0,0 +1,20 @@
+// Variable
+// Un espacio reservado en memoria, tiene un nombre y tipo de dato.
+
+// Primitivas
+// Número
+var x = 42;
+
+// Booleanos
+var desgraduado = false;
+
+// String
+var nombre = "Mel";
+
+// undefined => Tratamos de llamar a una variable que no existe
+// null => La variable no tiene un dato
+// NaN => Not a number (Esto no es un número)
+
+console.log(nombre);
+console.log(x);
+console.log(desgraduado);
